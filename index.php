4862739d16d78890c8cbcb2b4936a82daccdc10c
DavidePeloso-dev <davide.peloso.e@gmail.com>
2024-03-06 22:20:28 +0100

    Add pwGenerator

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
+$pwLen = $_GET['lenght'];
 
 
+function pwGenerator($len)
+{
+    $componets = [
+        'abcdefghijklmnopqrstuvwxyz',
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        '0123456789',
+        '!$%&?#',
+    ];
+    $pw = '';
+    for ($i = 0; $i < $len; $i++) {
+        $elemType = rand(0, count($componets) - 1);
+        $elem = $componets[$elemType];
+        $elemLen = strlen($elem);
+        $pwElemRand = rand(0, $elemLen - 1);
+        $pwElem = $elem[$pwElemRand];
+        //var_dump($elemType, $elem, $elemLen, $pwElemRand);
+        $pw .= $pwElem;
+    }
+    return $pw;
+}
+
+$password = pwGenerator($pwLen);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +35,12 @@
 </head>
 
 <body>
-
+    <form action="" method="get">
+        <label for="lenght">Digit the lenght for your password</label>
+        <input type="number" name="lenght" id="lenght">
+        <button type="submit">Generate</button>
+    </form>
+    <div>Your password is: <?= $password ?></div>
 </body>
 
 </html>
